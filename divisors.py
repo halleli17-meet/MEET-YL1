c047dffeec8836228257ed3283fc9ed96bdd517e
brit13-meet <brit13@meet.mit.edu>
2016-01-06 20:14:07 +0200

    ps

---
+++ divisors.py
@@ -0,0 +1,5 @@
+def buba(number):
+	for num in range(1, number):
+		if number % num==0:
+			print(num)
+buba(20)
---
+++ palindrome.py
@@ -0,0 +1,11 @@
+n=input()
+size_of_string= len(n)
+x=len(n)-1
+c=""
+while x>0:
+	c=c+n[x]
+	x=x+1
+if c==n:
+	print("True")
+else:
+	print("false")
