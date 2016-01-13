21fa2a8be0e517e60e90688ad06d5aa2412c6ed6
brit13-meet <brit13@meet.mit.edu>
2016-01-13 20:00:07 +0200

    oop

---
+++ animal_file.py
@@ -0,0 +1,8 @@
+class Animal (object):
+	def __init__(self,name,color,age):
+		self.color=color
+		self.name=name
+		self.age=age
+
+		def malak(self):
+			print(self.name +" is " + str)
---
+++ yousef.py
@@ -0,0 +1,8 @@
+from animal_file import Animal
+dog= Animal("Max", "Brown", 2)
+cat= Animal("Min", "White", 17)
+
+
+
+print(dog.age)
+print(cat.age)
